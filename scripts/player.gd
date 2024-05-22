86f1a7082e7a600d49a73349be177ad1a187f2eb
CWmain <cameronwhite2478@gmail.com>
2024-05-22 23:13:33 +1000

    Got air and ground movement in a way i like

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,15 +4,16 @@ const MAX_SPEED = 1000
 const ACC = 2000
 const DCC = 1500
 
-const MAX_AIR_SPEED = 2000
-const AIR_ACC = 1000
-const AIR_DCC = 500
-
-
+const MAX_AIR_SPEED = 2500
+const AIR_DIRECTION_CHANGE_MULTI = 2
+const AIR_ACC = 600
+const AIR_DCC = 400
 
 const SPEED = 50000.0
 const AIR_SPEED = 50000.0
 const JUMP_VELOCITY = -800.0
+
+@onready var speed_label = $SpeedLabel
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -32,9 +33,14 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Handle wall jump
 	if Input.is_action_just_pressed("jump") and wallJumpDirection != 0:
 		velocity.y = JUMP_VELOCITY
 		velocity.x = SPEED  * wallJumpDirection * delta
+
+	# Handle hold down
+	if Input.is_action_just_pressed("down") and !is_on_floor():
+		velocity.y = -JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -42,11 +48,13 @@ func _physics_process(delta):
 
 	if is_on_floor():
 		ground_velocity(direction, delta)
+	else:
+		air_velocity(direction, delta)
 	
-	
-
+	speed_label.text = "%v" % velocity
 	move_and_slide()
 
+# Directly modifies velocity.x
 func ground_velocity(direction, delta):
 	if (direction == 0):
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, DCC * delta)
@@ -57,27 +65,29 @@ func ground_velocity(direction, delta):
 	else:
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACC * delta)
 
+# Directly modifies velocity.x
+func air_velocity(direction, delta):
+	if (direction == 0):
+		velocity.x = move_toward(velocity.x, direction * MAX_AIR_SPEED, AIR_DCC * delta)
+	
+	elif sign(direction) != sign(velocity.x):
+		velocity.x = move_toward(velocity.x, direction * MAX_AIR_SPEED, (AIR_DCC+AIR_ACC) * AIR_DIRECTION_CHANGE_MULTI * delta)
+	
+	else:
+		velocity.x = move_toward(velocity.x, direction * MAX_AIR_SPEED, AIR_ACC * delta)
 
 func _on_wall_jump_detector_left_body_entered(body):
 	wallJumpDirection = 1
 	print("left wall jump entered ", body)
-	
-
-
 
 func _on_wall_jump_detector_left_body_exited(body):
 	wallJumpDirection = 0
 	print("left wall jump exited ", body)
 
-
 func _on_wall_jump_detector_right_body_entered(body):
 	wallJumpDirection = -1
 	print("right wall jump entered ", body)
 
-
 func _on_wall_jump_detector_right_body_exited(body):
 	wallJumpDirection = 0
 	print("right wall jump exited ", body)
-
-
-
