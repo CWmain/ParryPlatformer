ccd4dfd8cbacf615b5bbfc94a0760ea2f1b44be7
CWmain <cameronwhite2478@gmail.com>
2024-05-23 17:54:10 +1000

    Have the hook behaviouring in a good way, just need to add grapple points / phys objects to implement to pull to and go to functionality

--- scripts/GrappleHook.gd
+++ scripts/GrappleHook.gd
@@ -7,13 +7,18 @@ extends Node2D
 @onready var _cross_hair = $CrossHair
 
 @onready var _rope = $Rope
+@onready var grapple_cooldown = $GrappleCooldown
 
 var isGrappling: bool = false
 var grappleOut: bool = false
+
 var _mouse_pos_normal: Vector2 = Vector2(1,0)
 var _grapple_normal: Vector2 = Vector2(1,0)
 
 @onready var _grapple_tip = $GrappleTip
+
+func _ready():
+	grapple_cooldown.stop()
 
 func _process(_delta):
 	_mouse_pos_normal = get_local_mouse_position().normalized()
@@ -23,42 +28,50 @@ func _process(_delta):
 	#print("Rope Size: ", _rope.size.y)
 	var distance_to_tip = _rope.position.distance_to(_grapple_tip.position)
 	if _rope.size.y != distance_to_tip:
-		_rope.rotation = _grapple_tip.rotation + PI
+		_rope.rotation = _rope.position.angle_to(_grapple_tip.position) + PI/2
 		_rope.size.y = distance_to_tip
 	
 func _physics_process(_delta):
-	
 	if isGrappling:
 		if _grapple_tip.position.distance_to(Vector2(0,0)) < _grapple_length:
 
 			grappleOut = true
 			_grapple_tip.velocity = _grapple_normal * _grapple_rope_speed
-			_grapple_tip.move_and_slide()
+			if _grapple_tip.move_and_collide(_grapple_tip.velocity * _delta):
+				print("Collision")
+				release_grapple()
 		
-		if _grapple_tip.is_on_floor():
-			isGrappling = false
 		# Reached max length
 		if _grapple_tip.position.distance_to(Vector2(0,0)) >= _grapple_length:
 			_grapple_tip.position = _grapple_normal*_grapple_length
-			isGrappling = false
+			release_grapple()
 
 	else:
 		if _grapple_tip.position.distance_to(Vector2(0,0)) > 50:
-			_grapple_tip.velocity = _grapple_normal * -_grapple_rope_speed
-			_grapple_tip.move_and_slide()
+			#_grapple_tip.rotation = _grapple_tip.position.angle_to(to_local(Vector2(0,0))) - PI/4
+			_grapple_tip.velocity = _grapple_tip.position.normalized() * -_grapple_rope_speed
+			_grapple_tip.move_and_collide(_grapple_tip.velocity * _delta)
 		else:
 			_grapple_tip.position = Vector2(0,0)
-			grappleOut = false
+			if grapple_cooldown.is_stopped():
+				grapple_cooldown.start()
 	
 
 func shoot_grapple():
 	if !grappleOut:
-		_grapple_tip.rotation = _cross_hair.position.angle() + PI/2
+		_grapple_tip.collision_mask = 4
+		_grapple_tip.rotation = _cross_hair.position.angle() + PI / 2
 		#Update shoot direction
 		_grapple_normal = _mouse_pos_normal.normalized()
 		#Toggle is grappling
 		isGrappling = true
 		
 func release_grapple():
+	#_grapple_tip.rotation = _grapple_tip.position.angle_to(Vector2(0,0)) + PI/2
 	isGrappling = false
+	_grapple_tip.collision_mask = 0
 	
+
+
+func _on_grapple_cooldown_timeout():
+	grappleOut = false
