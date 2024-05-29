760205d572f461f9dfa8a54c9ed2787e2f78f2e6
CWmain <cameronwhite2478@gmail.com>
2024-05-29 23:56:13 +1000

    Fixed issue with bullet rotating when gun rotates

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
-var rotation_speed = 5
+var rotation_speed = PI/8
 var bullet_type = preload("res://objects/bullet.tscn") 
+
+func _physics_process(delta):
+	rotation += rotation_speed*delta
 
 func fire_gun():
 	var b = bullet_type.instantiate()
-	add_child(b)
-	b.velocity = b.velocity.length() * Vector2.from_angle(b.rotation)
+	
+	# Add as child of parent (which would be the level scene) to avoid the bullet rotating with gun	
+	get_parent().add_child(b)
+
+	# Set the velocity to be at the correct angle from the gun
+	b.velocity = b.velocity.length() * Vector2.from_angle(rotation)
+	# Set the position to be the front of the gun in the global space (otherwise spawns at (0,0)
+	b.position = to_global(b.position)
+
 	
 
 
