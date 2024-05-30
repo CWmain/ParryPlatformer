f49d693a17736350c60a4b25304efcc5c929f5ec
CWmain <cameronwhite2478@gmail.com>
2024-05-30 21:36:17 +1000

    Allow basic foe to detect / track player location when in cone. Rotates gun to aim / shoot at player

--- scripts/BasicFoe.gd
+++ scripts/BasicFoe.gd
@@ -1,9 +1,24 @@
 extends Pullable
 
+@onready var gun = $Gun
+
+var trackPlayer: Object = null
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta * 2
-	
+		velocity.y += gravity * delta
+	if trackPlayer:
+		print(trackPlayer.position)
+		gun.rotation = to_global(gun.position).angle_to_point(trackPlayer.position)
 	move_and_slide()
+
+
+func _on_player_detector_right_body_entered(body):
+	trackPlayer = body
+
+
+
+func _on_player_detector_right_body_exited(body):
+	#When exited detection cone, stop tracking player
+	trackPlayer = null
--- scripts/gun.gd
+++ scripts/gun.gd
@@ -2,19 +2,29 @@ extends Node2D
 
 var rotation_speed = PI/8
 var bullet_type = preload("res://objects/bullet.tscn") 
+@export var firerate: float  = 1
+@onready var gun_sprite = $GunSprite
+@onready var fire_gun_timer = $FireGunTimer
+
+func _ready():
+	fire_gun_timer.wait_time = firerate
 
 func _physics_process(delta):
-	rotation += rotation_speed*delta
+	pass#rotation += rotation_speed*delta
 
 func fire_gun():
 	var b = bullet_type.instantiate()
 	
 	# Add as child of parent (which would be the level scene) to avoid the bullet rotating with gun	
-	get_parent().add_child(b)
+	get_tree().get_root().add_child(b)
 
 	# Set the velocity to be at the correct angle from the gun
 	b.velocity = b.velocity.length() * Vector2.from_angle(rotation)
-	# Set the position to be the front of the gun in the global space (otherwise spawns at (0,0)
+	
+	
+	# Set the position to be the front of the gun in the global space (otherwise spawns at (0,0))
+	var gunLength: int = gun_sprite.get_rect().size.x
+	b.position.x += gunLength
 	b.position = to_global(b.position)
 
 	
